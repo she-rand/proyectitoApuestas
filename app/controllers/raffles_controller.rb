ee0cc36be66e251de08cfb2d72c01f1f4bae885a
Lili <lzurita@moit.cl>
2022-02-21 11:51:45 -0300

    feat:calc bet amout randomly

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -9,23 +9,29 @@ class RafflesController < ApplicationController
   def new
     @raffle=Raffle.new
   end
+
   def create
+    color=random_color()
     @raffle=Raffle.new({color:color})
     @players=Player.all
+
     @players.each do |player|
-      color=randomcolor()
-
-      @bets=@raffle.bets.new({bet_color:color,bet_amount:"6000", player:player})
-
+      color=random_color()
+      @transfers=player.transfers
+      bet_amount=Transfer.calc_bet_amount(@transfers)
+      if bet_amount>0
+        @bet=@raffle.bets.new({bet_color:color,bet_amount:bet_amount, player:player})
+      end
     end
+
     if @raffle.save
-      @raffle.bets.build(player:@players[0],bet_color:color,bet_amount:'6000')
       redirect_to '/raffles'
     else
       render '/new'
     end
   end
-  def randomcolor
+
+  def random_color
     nrandom=rand(99)
     if nrandom==0||nrandom==1
       color='verde'
@@ -36,7 +42,7 @@ class RafflesController < ApplicationController
     end
     color
   end
-  
+
   private
   def raffle_params
     params.require(:raffle).permit(:color)
