8da4f60e07dc593bd710114325494810bd2bc6e3
Lili <lzurita@moit.cl>
2022-02-23 01:45:39 -0300

    feat:seed created using api and creating weather record

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+weather=Weather.forecast_from_api()
 players=Player.create([{first_name:"Eufranio",last_name:"Perez",rut:"1-1"},{first_name:"Anatolio",last_name:"Burgos",rut:"1-1"},{first_name:"Gerónimo",last_name:"Martínez",rut:"3-3"},{first_name:"Martín",last_name:"Rivas",rut:"4-4"}])
-raffles=Raffle.create([{color:"rojo"},{color:"rojo"},{color:"verde"},{color:"negro"},{color:"verde"},{color:"negro"}])
+raffles=Raffle.create([{color:"rojo",weather:weather},{color:"rojo",weather:weather},{color:"verde",weather:weather},{color:"negro",weather:weather},{color:"verde",weather:weather},{color:"negro",weather:weather}])
 bets=Bet.create([{bet_amount:"4000",bet_color:"rojo",player:players[0],raffle:raffles[0]}])
 
 transfers=Transfer.create([{amount:"10000",player:players[0]},{amount:"12000",player:players[0]}])
