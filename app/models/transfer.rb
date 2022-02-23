32eb75341732a557495417dc4dde0805ace954d8
Lili <lzurita@moit.cl>
2022-02-23 01:41:50 -0300

    feat:calc bet amount using temp

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -10,16 +10,26 @@ class Transfer < ApplicationRecord
 
   def self.calc_bet_amount(transfers)
     total=self.balance(transfers)
+    weather=Weather.forecast_today()
+    temp=weather.nil? ? nil : weather.forecast_temp
     bet_amount=0
+    nrandom=rand(99)
+
     if  total==0
       bet_amount=0
     elsif total<=1000
       betamount=total
-    else
-      nrandom=rand(99)
-      percentage=(7.0/99.0)*(nrandom-8)+8
+    elsif temp<=29
+      percentage=(7.0/99.0)*nrandom+8
       factor=percentage/100.0
       bet_amount=total*factor
+    elsif temp>29
+      percentage=(4.0/99.0)*nrandom+3
+      factor=percentage/100.0
+      bet_amount=total*factor
+    elsif temp.nil
+      puts "Ocurrió un error con la temperatura"
+
     end
     bet_amount
     end
