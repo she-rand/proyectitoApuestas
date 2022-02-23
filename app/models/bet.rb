feea695e28c904828a465b66dbe71ef1c4bec021
Lili <lzurita@moit.cl>
2022-02-23 01:41:18 -0300

    feat:pay_bet method

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,4 +1,18 @@
 class Bet < ApplicationRecord
   belongs_to :player
   belongs_to :raffle
+  def self.pay_bet(bet)
+    @player=bet.player
+    @raffle=bet.raffle
+    transfer_amount=0
+
+    if(@raffle.color==bet.bet_color)
+      transfer_amount=bet.bet_amount.to_i
+    else
+      transfer_amount=bet.bet_amount.to_i*(-1)
+
+    end
+    @transfer=@player.transfers.create({amount:transfer_amount})
+
+  end
 end
