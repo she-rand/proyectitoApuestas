2694c4ab6960eaf77a7646664e801833b246ed55
Lili <lzurita@moit.cl>
2022-02-23 01:42:30 -0300

    feat: all raffle process created

--- app/models/raffle.rb
+++ app/models/raffle.rb
@@ -1,4 +1,40 @@
 class Raffle < ApplicationRecord
   has_many :bets
   has_many :players, through: :bets
+  belongs_to :weather
+
+  def self.random_color
+    nrandom=rand(99)
+    if nrandom==0||nrandom==1
+      color='verde'
+    elsif nrandom>1&& nrandom<50
+      color='rojo'
+    else
+      color='negro'
+    end
+    color
+  end
+
+  def self.all_raffle_process
+    @weather=Weather.forecast_today()
+
+    color=Raffle.random_color()
+    @raffle=Raffle.create({color:color,weather:@weather})
+    @players=Player.all
+
+    @players.each do |player|
+      color=Raffle.random_color()
+
+
+      @transfers=player.transfers
+      bet_amount=Transfer.calc_bet_amount(@transfers)
+      if bet_amount>0
+        @bet=@raffle.bets.create({bet_color:color,bet_amount:bet_amount, player:player})
+        Bet.pay_bet(@bet)
+        #@transfer=Transfer.create({amount:"10074",player:player})
+
+
+      end
+    end
+  end
 end
