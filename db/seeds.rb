b229fc2e6a7d0e40cd85de05ac40999d45567778
Lili <lzurita@moit.cl>
2022-02-23 20:05:56 -0300

    feat:seed players begin having 10000

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 weather=Weather.forecast_from_api()
-players=Player.create([{first_name:"Eufranio",last_name:"Perez",rut:"1-1"},{first_name:"Anatolio",last_name:"Burgos",rut:"1-1"},{first_name:"Gerónimo",last_name:"Martínez",rut:"3-3"},{first_name:"Martín",last_name:"Rivas",rut:"4-4"}])
-raffles=Raffle.create([{color:"rojo",weather:weather},{color:"rojo",weather:weather},{color:"verde",weather:weather},{color:"negro",weather:weather},{color:"verde",weather:weather},{color:"negro",weather:weather}])
-bets=Bet.create([{bet_amount:"4000",bet_color:"rojo",player:players[0],raffle:raffles[0]}])
+players=Player.create([{first_name:"Eufranio",last_name:"Perez",rut:"1-1"},{first_name:"Anatolio",last_name:"Burgos",rut:"1-1"},{first_name:"Sol",last_name:"Mar",rut:"3-3"},{first_name:"Juanin",last_name:"Harry",rut:"4-4"}])
 
-transfers=Transfer.create([{amount:"10000",player:players[0]},{amount:"12000",player:players[0]}])
+transfers=Transfer.create([{amount:"10000",player:players[0]},{amount:"10000",player:players[1]},{amount:"10000",player:players[2]},{amount:"10000",player:players[3]}])
